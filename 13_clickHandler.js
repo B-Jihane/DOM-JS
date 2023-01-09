2ef07a17f08b7c84d17877a01643d105ff92482d
Jihane BELKHOU <jihane.belkhou@outlook.fr>
2023-01-09 16:17:37 +0100

    suites exos

--- 13_clickHandler.js
+++ 13_clickHandler.js
@@ -1,5 +1,5 @@
 let button = document.querySelector("button#clickme");
-button.addEventListener('click', (e) => {
+button.addEventListener('click', () => {
     let p = document.createElement('p');
     p.textContent = 'clicked';
     document.querySelector("div.content").appendChild(p);
