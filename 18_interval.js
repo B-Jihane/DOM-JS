d96239c8b213ea0d4b169affadcbd02d7256d26d
Jihane BELKHOU <jihane.belkhou@outlook.fr>
2023-01-11 16:09:50 +0100

    suite exo events

--- 18_interval.js
+++ 18_interval.js
@@ -1,9 +1,15 @@
 function horloge(){
     let date = new Date();
-    let day = date.getDay();
-    let month = date.getMonth();
+    let day = date.getDate();
+    let month = date.getMonth()+1;
     let year = date.getFullYear();
     let hour = date.getHours();
     let minute = date.getMinutes();
     let second = date.getSeconds();
+    p.innerHTML = day +"/" +month+"/" +year+" " +hour+":" +minute+":" +second ;
+    document.body.appendChild(p);
 }
+
+let p = document.createElement("p");
+
+setInterval(horloge, 1000);
