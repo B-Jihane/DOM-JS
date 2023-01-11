ee6cdd77b314d0b57015ef17e9cd2fe32a9aa948
Jihane BELKHOU <jihane.belkhou@outlook.fr>
2023-01-11 09:46:21 +0100

    suite exo events

--- 16_contactForm.js
+++ 16_contactForm.js
@@ -1,28 +1,14 @@
-const inputs = document.getElementsByTagName("input");
-let pArray = [];
-for (let i = 0; i < inputs.length; i++) {
-    pArray.push(inputs[i].name + ": " + inputs[i].value);
-  }
+let nom = document.getElementsByName('name')[0].value;;
+let age = document.getElementsByName("age")[0].value;
+let birthdate = document.getElementsByName("birthdate")[0].value;
 
 const p = document.createElement('p');
 p.id = "result";
-p.textContent = pArray.toString();
+p.textContent = "name: " + nom + ", age: " + age + ", birthdate: " + birthdate ;
 
 const content = document.querySelector('body');
 content.insertBefore(p, content.lastChild);
 
 
-
-
-/* let nom = inputs["name"];
-let age = inputs["age"];
-let birthdate = inputs["birthdate"];
-
-const p = document.createElement('p');
-p.id = "result";
-p.textContent = 'name: ' + nom +', age: ' + age + ', birthdate: ' + birthdate ;
-
-const content = document.querySelector('body');
-content.insertBefore(p, content.lastChild); */
 
 
