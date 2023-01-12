d1b2762e57f55988f7d0bffd7eae993fab31ec98
Jihane BELKHOU <jihane.belkhou@outlook.fr>
2023-01-12 13:38:33 +0100

    exercices asynchrone

---
+++ 24_myPromiseTimeout_await.js
@@ -0,0 +1,13 @@
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
+const button = document.querySelector('button');
+button.addEventListener('click', async () => {
+    await sleep(2000)
+    .then(() => {
+        let p = document.createElement('p');
+        p.textContent = 'add-me';
+        document.body.appendChild(p);
+    });
+});
