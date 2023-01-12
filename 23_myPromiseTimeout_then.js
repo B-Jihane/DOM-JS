f100d1842b21987a14f36b2b82214fac26535a2b
Jihane BELKHOU <jihane.belkhou@outlook.fr>
2023-01-12 12:16:06 +0100

    exercices asynchrone

--- 23_myPromiseTimeout_then.js
+++ 23_myPromiseTimeout_then.js
@@ -1,9 +1,13 @@
-const btn = document.querySelector("button");
-btn.addEventListener("click", async function (){
-    await new Promise(resolve => setTimeout(resolve, 2000))
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
+const button = document.querySelector('button');
+button.addEventListener('click', async () => {
+    await sleep(2000)
     .then(() => {
-        let p = document.createElement("p");
-        p.textContent = "add-me";
+        let p = document.createElement('p');
+        p.textContent = 'add-me';
         document.body.appendChild(p);
     });
 });
